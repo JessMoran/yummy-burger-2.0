0930cafd1273a8a5aa5971468f6340e354ce85db
Jess Moran <jessmoran.dev@gmail.com>
2020-07-29 23:30:31 -0400

    Add heroku settings

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,30 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
-
+/*
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "JessMoran29",
   database: "yummy_burger_db"
-});
-/*let connection;
+}); */
 
-if (process.env.AWSDB_URL) {
+let connection;
+
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 }else {
   connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
     user: "root",
     password: "JessMoran29",
     database: "yummy_burger_db"
   })
 
   connection.connect();
-}*/
+}
 
-
+/*
 // Make connection.
 connection.connect(function(err) {
   if (err) {
@@ -32,7 +32,7 @@ connection.connect(function(err) {
     return;
   }
   console.log("connected as id " + connection.threadId);
-});
+}); */
 
 // Export connection for our ORM to use.
 module.exports = connection;
