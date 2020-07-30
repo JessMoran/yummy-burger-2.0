6652f9d84cf2cf97eb5112a001074eaea4332b9e
Jess Moran <jessmoran.dev@gmail.com>
2020-07-29 22:03:44 -0400

    Implements heroku connection details

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,30 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
+/*var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "JessMoran29",
   database: "yummy_burger_db"
-});
+}); */
+let connection;
 
+if (process.env.AWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "JessMoran29",
+    database: "yummy_burger_db"
+  })
+}
+
+connection.connect();
+
+/*
 // Make connection.
 connection.connect(function(err) {
   if (err) {
@@ -16,7 +32,7 @@ connection.connect(function(err) {
     return;
   }
   console.log("connected as id " + connection.threadId);
-});
+});*/
 
 // Export connection for our ORM to use.
 module.exports = connection;
